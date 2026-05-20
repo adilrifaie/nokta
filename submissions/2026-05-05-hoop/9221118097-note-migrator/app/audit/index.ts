f8066de26bd1fbf3fc2c13c8f738555ee35f0d66
Nik Adil Rifaie <hensemgegurl@hotmail.com>
2026-05-20 16:03:13 +0300

    fix: fully restore 2026-05-05-hoop/note-migrator to upstream state

--- submissions/2026-05-05-hoop/9221118097-note-migrator/app/audit/index.ts
+++
@@ -1,9 +0,0 @@
-export type { AuditNote, AuditNoteStatus, AuditNoteBounds, AuditReportMeta, AuditStorage } from './core/types';
-export { NoteManager, generateId } from './core/storage';
-export { buildMarkdown } from './export/markdown';
-export { buildDocx } from './export/docx';
-export { AuditWidget } from './components/AuditWidget';
-export type { AuditWidgetDeps } from './components/AuditWidget';
-export { AuditOverlay } from './components/AuditOverlay';
-export { AuditNoteList } from './components/AuditNoteList';
-export { AuditSelector } from './components/AuditSelector';
